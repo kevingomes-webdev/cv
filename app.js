050daab879a75cf742a417b796349f54ed125aec
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-12 20:50:48 +0200

    c

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
+/**const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+ */
 const app = express();
 
-const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+/*app.set("views", path.join(__dirname, "views"));*/
 
 
 app.get("/", (req, res) => {
@@ -31,6 +31,7 @@ app.get("/de", (req, res) => {
     res.render("pages/de");
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
