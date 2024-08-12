3ed7fc88ae6d45694d7d2a1ea3211d4cbd7f7686
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-12 18:33:34 +0200

    c

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("pages/index.ejs");
 });
 
 app.listen(port, () => {
