9cca217b4a20e7e10e109ac6bc005a8ddadcd1d0
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-12 16:27:11 +0200

    changes

--- Public/cv.js
+++ Public/cv.js
@@ -1,3 +1,22 @@
+/** OffCanvas Menu */
+
+$(document).ready(function() {
+    // Select the menu toggle and off-canvas menu
+    const menuToggle = $('.menu-toggle');
+    const offCanvasMenu = $('.off-canvas-menu');
+    const closeBtn = $('.close-btn');
+
+    // Show the off-canvas menu when the toggle button is clicked
+    menuToggle.on('click', function() {
+        offCanvasMenu.addClass('active');
+    });
+
+    // Hide the off-canvas menu when the close button is clicked
+    closeBtn.on('click', function() {
+        offCanvasMenu.removeClass('active');
+    });
+});
+
 /** Date ****/
 
 const today = new Date().getFullYear();
