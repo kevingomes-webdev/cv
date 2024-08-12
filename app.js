3d93ebb50127d1fe2a57177836eaf0383651e064
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-12 18:52:47 +0200

    c

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
 
-app.set("view engine", "ejs");
+// Set the directory for the views (EJS templates)
 app.set('views', path.join(__dirname, 'views'));
 
-app.get("/", (req, res) => {
-  res.render("pages/index.ejs");
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index'); // Renders 'index.ejs' from the 'views' directory
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
