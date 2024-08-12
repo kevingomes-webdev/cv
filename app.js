e2e68da6195bd9cc25547f61ff5995d1aa4f4c0b
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-12 18:09:31 +0200

    changes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 import express from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render("index.ejs");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
