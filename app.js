b28fa7b31c634b4ac693247db4a387108a8f53cc
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-13 02:57:43 +0200

    c

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(express.static(path.join(__dirname, "Public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
