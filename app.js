f53b50fec0367b52749d92e1ea3edccc8e53750f
kevingomes-webdev <gomeskevin.office@gmail.com>
2024-08-13 11:46:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 /**Middleware */
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
